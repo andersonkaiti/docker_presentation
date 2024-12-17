64ad0e9d81f2289cd41f104da69c214199369578
andersonkaiti <anderkaiti@gmail.com>
2024-12-17 18:23:36 -0300

    :sparkles: feat: Quiz realizado.

---
+++ exerc-01/ex-01/lab-01/run.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import time
+
+app = Flask(__name__)
+
+time.sleep(3)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://username:password@db/dbname'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    name = request.json['name']
+    new_user = User(name=name)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added'}), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name} for user in users])
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=False)
